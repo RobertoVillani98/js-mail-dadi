d88ca29193397e9ac767f643072dc536c68a07f5
RobertoVillani <robertovillani58@gmail.com>
2021-11-10 16:48:37 +0100

    gioco dadi dopo verifica mail. COMMENTATO

--- mail/js/script.js
+++ mail/js/script.js
@@ -13,6 +13,7 @@ let sentinella = false;
 for (let i = 0; i < mailUtente.length && sentinella == false; i++) {
  if (mail[i] == mailUtente) {
   sentinella = true;
+  console.log("Utente trovato");
  } else {
   sentinella = false;
  }
@@ -20,6 +21,29 @@ for (let i = 0; i < mailUtente.length && sentinella == false; i++) {
 
 if (sentinella) {
  alert("utente trovato: puoi accedere");
+
+ //  ******** gioco dei dadi inserito dopo la verifica della mail
+ //  alert("inizio gioco dei dadi");
+ //  let user;
+ //  let pc;
+
+ //  user = Math.floor(Math.random() * 6) + 1;
+ //  console.log("il dato dell'utente ha come risultato: " + user);
+
+ //  pc = Math.floor(Math.random() * 6) + 1;
+ //  console.log("il dato del pc ha come risultato: " + pc);
+
+ //  if (user > pc) {
+ //   alert("User Win!");
+ //   console.log("L'utente ha vinto");
+ //  } else if (user < pc) {
+ //   alert("Pc win!");
+ //   console.log("Il Pc ha vinto");
+ //  } else if ((user = pc)) {
+ //   alert("Pareggio");
+ //   console.log("Pareggio");
+ //  }
 } else {
  alert("utente non trovato: non puoi accedere");
+ console.log("Utente non trovato");
 }
