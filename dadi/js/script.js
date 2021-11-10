fba30a5709905c39c9ffe775b4462d61c8e68481
RobertoVillani <robertovillani58@gmail.com>
2021-11-10 16:35:35 +0100

    dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,19 @@
+let user;
+let pc;
+
+user = Math.floor(Math.random() * 6) + 1;
+console.log("il dato dell'utente ha come risultato: " + user);
+
+pc = Math.floor(Math.random() * 6) + 1;
+console.log("il dato delpc ha come risultato: " + pc);
+
+if (user > pc) {
+ alert("User Win!");
+ console.log("L'utente ha vinto");
+} else if (user < pc) {
+ alert("Pc win!");
+ console.log("Il Pc ha vinto");
+} else if ((user = pc)) {
+ alert("Pareggio");
+ console.log("Pareggio");
+}
